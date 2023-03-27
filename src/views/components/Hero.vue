7975b7148bcb96af40aec9dce11150285ccb9b92
chacklong <3066199080@qq.com>
2023-03-27 21:29:58 +0800

    first commit

---
+++ src/views/components/Hero.vue
@@ -0,0 +1,63 @@
+<template>
+    <section class="section-hero section-shaped my-0">
+        <div class="shape shape-style-1 shape-primary">
+            <span class="span-150"></span>
+            <span class="span-50"></span>
+            <span class="span-50"></span>
+            <span class="span-75"></span>
+            <span class="span-100"></span>
+            <span class="span-75"></span>
+            <span class="span-50"></span>
+            <span class="span-100"></span>
+            <span class="span-50"></span>
+            <span class="span-100"></span>
+        </div>
+        <div class="container shape-container d-flex align-items-center">
+            <div class="col px-0">
+                <div class="row justify-content-center align-items-center">
+                    <div class="col-lg-7 text-center pt-lg">
+                        <!-- <img src="img/brand/white.png" style="width: 200px;" class="img-fluid"> -->
+                        <h1 class="display-1 text-white">新型IT技术服务中心</h1>
+                        <p class="lead text-white mt-4 mb-5">我们是专家，为您的业务发展提供最广泛的IT服务和解决方案。</p>
+                        <div class="btn-wrapper">
+                            <base-button tag="a"
+                                         href="https://demos.creative-tim.com/vue-argon-design-system/documentation"
+                                         class="mb-3 mb-sm-0"
+                                         type="default"
+                                         >
+                                开始咨询
+                            </base-button>
+                        </div>
+                    </div>
+                </div>
+                <div class="row align-items-center justify-content-around stars-and-coded">
+                    <div class="row nav-tag">
+                        <div class="col-sm">
+                            <span>产品与技术服务</span>
+                        </div>
+                        <div class="col-sm">
+                            <span>行业解决方案分析</span>
+                        </div>
+                        <div class="col-sm">
+                            <span>企业团队协作</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+export default {};
+</script>
+<style>
+.section-hero {
+    background-image: url(../../../public/hero-bg.png);
+    background-size: cover;
+    background-repeat: no-repeat;
+}
+.col-sm{
+    width: 158px !important;
+    margin-top: 200px;
+}
+</style>
---
+++ src/views/components/Inputs.vue
@@ -0,0 +1,59 @@
+<template>
+    <section class="position-relative py-5 bg-dark">
+        <span class="position-absolute top-0 start-0 d-dark-mode-block d-none w-100 h-100 bg-secondary"></span>
+        <div class="position-relative dark-mode container my-lg-5 my-md-4 my-3 py-xl-3">
+          <h2 class="h1 mb-md-4 mb-3 text-center">title.demo</h2>
+          <p class="mb-md-5 mb-4 pb-xl-3 pb-lg-2 pb-md-0 pb-sm-2 mx-auto text-center" style="max-width: 32.875rem;">this.title</p>
+            <!-- Swiper item -->
+            <div class="row card-bg">
+                <div class="card-menu-news">
+                    <div class="col card-title-font">
+                        <span class="display-3">demo title</span>
+                    </div>
+                    <div class="card-content">
+                       <blockquote class="lockquote">
+                        <p class="mb-0">this my text content-box</p>
+                       </blockquote>
+                    </div>
+                </div>
+            </div>
+            <!-- Swiper pagination -->
+            <div class="swiper-pagination position-static mt-4 pt-lg-3 pt-2 swiper-pagination-clickable swiper-pagination-bullets swiper-pagination-horizontal swiper-pagination-lock" _mstvisible="0"><span class="swiper-pagination-bullet swiper-pagination-bullet-active"></span>
+            </div>
+          <span class="swiper-notification"></span>
+        </div>
+      </section>
+</template>
+<script>
+export default {};
+</script>
+<style>
+.swiper-wrapper {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+    display: flex;
+    transition-property: transform;
+    box-sizing: content-box;
+}
+.card-bg{
+    background-color: hsla(0, 0%, 95%, 0.548);
+}
+.card-menu-news{
+    width: 300px;
+    height: 450px;
+    border: 1px #472ddb solid;
+    border-radius: 12px;
+}
+.card-title-font{
+    display: block;
+    text-align: center;
+    margin-top: 17px;
+}
+.card-content{
+    display: block;
+    text-align: center;
+    margin-top: 22px;
+}
+</style>
